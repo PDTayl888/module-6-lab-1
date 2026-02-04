6cccb723d05a6600bc1cd12d2ec63c96662ba0b0
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-02-03 19:22:51 -0500

    added DISPLAY DEAILS

--- inventory-tracker/src/main.ts
+++ inventory-tracker/src/main.ts
@@ -1,6 +1,7 @@
 import { DigitalProduct } from './models/DigitalProduct.js';
 import { PhysicalProduct } from './models/PhysicalProduct.js';
 
+
 const products = [
     new DigitalProduct("gff4323", "Bunk Beds", 3000, 1200),
     new PhysicalProduct("eew985", "Rocking Chair", 1200, 1432),
@@ -13,3 +14,10 @@ const products = [
     new DigitalProduct("gff4323", "Bunk Beds", 3000, 1200),
     new PhysicalProduct("eew985", "Rocking Chair", 1200, 1432),
 ]
+
+for (const product of products) {
+    console.log("PRODUCT DETAILS:");
+    console.log(product.displayDetails());
+
+    
+}
